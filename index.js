d284f5cfb1589dcdd6dbb680a7a477e5ec5d463f
Maksim <gato-v@yandex.ru>
2021-10-19 21:53:54 +0300

    Added output of prime numbers from the range

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 require('colors')
 
+const leftRest = process.argv[2];
+const rightRest = process.argv[3];
 
-if(isNaN(process.argv[2]) || isNaN(+process.argv[3])){
+if(isNaN(leftRest) || isNaN(rightRest)){
     console.log('incorrect start parameters'.red);
     return;
 }
 
+const isPrimeNum = (num) => {
+    if (num < 1)
+        return false;
+    for(let i = 2; i < num; i++)
+        if(num % i === 0) return false;
+    return true;
+}
 
-console.log('hello'.green);
+for (let i = leftRest; i <= rightRest; i++){
+    isPrimeNum(i) && console.log(i);
+}
 
