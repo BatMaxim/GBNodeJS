e78042d3e398b75520ce7145c6d5020f3997928c
Maksim <gato-v@yandex.ru>
2021-11-04 14:38:47 +0300

    Added directory navigation

--- cli.js
+++ cli.js
@@ -4,27 +4,30 @@ const path = require('path');
 const inquirer = require('inquirer');
 
 const isFile = fileName => fs.lstatSync(fileName).isFile();
+let currentDir = process.cwd();
 
-const currentDir = process.cwd();
+const explorer = () => {
+    const filesList = ["../", ...fs.readdirSync(currentDir)];
+    inquirer.prompt([
+        {
+            name: 'fileName',
+            type: 'list', // input, number, confirm, list, checkbox, password
+            message: 'Choose the file to read',
+            choices: filesList,
+        }
+    ])
+        .then(answer => answer.fileName)
+        .then(fileName => {
+            const fullPath = path.resolve(currentDir, fileName);
+            if(isFile(fullPath)) {
+                const data = fs.readFileSync(fullPath, 'utf-8');
+                console.log(data);
+            } else
+            {
+               currentDir = fullPath;
+               explorer();
+            }
+        });
+}
 
-const filesList = fs.readdirSync(currentDir);
-inquirer.prompt([
-    {
-        name: 'fileName',
-        type: 'list', // input, number, confirm, list, checkbox, password
-        message: 'Choose the file to read',
-        choices: filesList,
-    }
-])
-    .then(answer => answer.fileName)
-    .then(fileName => {
-        console.log(fileName);
-        // const fullPath = path.resolve(executionDir, fileName);
-        // if(isFile(fullPath)) {
-        //     const data = fs.readFileSync(fullPath, 'utf-8');
-        //     console.log(data);
-        // } else
-        // {
-        //     console.log("is not a file");
-        // }
-    });
+explorer();
