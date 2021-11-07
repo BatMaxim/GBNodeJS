7d60920b23e5f098c8a26b39aa961cd8cc7dc50b
Maksim <gato-v@yandex.ru>
2021-11-07 22:43:53 +0300

    added backward transition

--- cli.js
+++ cli.js
@@ -4,65 +4,24 @@ const http = require('http');
 const path = require('path');
 const yargs = require('yargs');
 const inquirer = require('inquirer');
-//
-// const options = yargs
-//     .positional('p',{
-//             describe: "Pattern",
-//             default: ""
-//         }
-//     ).argv;
-//
-// const isFile = fileName => fs.lstatSync(fileName).isFile();
-// let currentDir = process.cwd();
-//
-// const explorer = () => {
-//     const filesList = ["../", ...fs.readdirSync(currentDir)];
-//     inquirer.prompt([
-//         {
-//             name: 'fileName',
-//             type: 'list', // input, number, confirm, list, checkbox, password
-//             message: 'Choose the file to read',
-//             choices: filesList,
-//         }
-//     ])
-//         .then(answer => answer.fileName)
-//         .then(fileName => {
-//             const fullPath = path.resolve(currentDir, fileName);
-//             if(isFile(fullPath)) {
-//                 const data = fs.readFileSync(fullPath, 'utf-8');
-//                 if(!options.p) console.log(data);
-//                 else {
-//                     const reg = RegExp(options.p, 'igm');
-//                     console.log(data.match(reg));
-//                 }
-//             } else
-//             {
-//                currentDir = fullPath;
-//                explorer();
-//             }
-//         });
-// }
-
-// explorer();
 
 http.createServer((req, res)=>{
     const isFile = fileName => fs.lstatSync(fileName).isFile();
-    let currentDir = path.join(process.cwd(), req.url);
+    let currentDir = path.join(process.cwd(), req.url).replace(/__back/g, "../");
+    console.log(currentDir);
     if(!fs.existsSync(currentDir))
        return res.end('File or directory nt found');
-
     if(isFile(currentDir))
         return fs.createReadStream(currentDir).pipe(res);
 
     let linksList = "";
-
+    linksList+= `<li><a href="${path.join(req.url, "__back")}">../</a></li>`
     fs.readdirSync(currentDir).forEach(el =>{
         const filePath = path.join(req.url, el);
         linksList+= `<li><a href="${filePath}">${el}</a></li>`
     });
 
 
-    let curPath = "1";
     const HTML = fs
         .readFileSync(path.join(__dirname, 'index.html'), 'utf-8')
         .replace('##path', currentDir)
