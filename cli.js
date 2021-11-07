aa2ac57464f09a2ef2fb8fb07eec1ebf08ffd182
Maksim <gato-v@yandex.ru>
2021-11-07 19:05:29 +0300

    added start logic

--- cli.js
+++ cli.js
@@ -1,45 +1,72 @@
 #!/usr/local/bin/node
 const fs = require('fs');
+const http = require('http');
 const path = require('path');
 const yargs = require('yargs');
 const inquirer = require('inquirer');
+//
+// const options = yargs
+//     .positional('p',{
+//             describe: "Pattern",
+//             default: ""
+//         }
+//     ).argv;
+//
+// const isFile = fileName => fs.lstatSync(fileName).isFile();
+// let currentDir = process.cwd();
+//
+// const explorer = () => {
+//     const filesList = ["../", ...fs.readdirSync(currentDir)];
+//     inquirer.prompt([
+//         {
+//             name: 'fileName',
+//             type: 'list', // input, number, confirm, list, checkbox, password
+//             message: 'Choose the file to read',
+//             choices: filesList,
+//         }
+//     ])
+//         .then(answer => answer.fileName)
+//         .then(fileName => {
+//             const fullPath = path.resolve(currentDir, fileName);
+//             if(isFile(fullPath)) {
+//                 const data = fs.readFileSync(fullPath, 'utf-8');
+//                 if(!options.p) console.log(data);
+//                 else {
+//                     const reg = RegExp(options.p, 'igm');
+//                     console.log(data.match(reg));
+//                 }
+//             } else
+//             {
+//                currentDir = fullPath;
+//                explorer();
+//             }
+//         });
+// }
 
-const options = yargs
-    .positional('p',{
-            describe: "Pattern",
-            default: ""
-        }
-    ).argv;
+// explorer();
 
-const isFile = fileName => fs.lstatSync(fileName).isFile();
-let currentDir = process.cwd();
+http.createServer((req, res)=>{
+    const isFile = fileName => fs.lstatSync(fileName).isFile();
 
-const explorer = () => {
-    const filesList = ["../", ...fs.readdirSync(currentDir)];
-    inquirer.prompt([
-        {
-            name: 'fileName',
-            type: 'list', // input, number, confirm, list, checkbox, password
-            message: 'Choose the file to read',
-            choices: filesList,
-        }
-    ])
-        .then(answer => answer.fileName)
-        .then(fileName => {
-            const fullPath = path.resolve(currentDir, fileName);
-            if(isFile(fullPath)) {
-                const data = fs.readFileSync(fullPath, 'utf-8');
-                if(!options.p) console.log(data);
-                else {
-                    const reg = RegExp(options.p, 'igm');
-                    console.log(data.match(reg));
-                }
-            } else
-            {
-               currentDir = fullPath;
-               explorer();
-            }
-        });
-}
+    let currentDir = path.join(process.cwd(), req.url);
+    if(!fs.existsSync(currentDir))
+        res.end('File or directory nt found');
 
-explorer();
+    if(isFile(currentDir))
+        return fs.createReadStream(currentDir).pipe(res);
+
+    
+
+    let linksList = "123";
+    let curPath = "1";
+    const HTML = fs
+        .readFileSync(path.join(__dirname, 'index.html'), 'utf-8')
+        .replace('##path', currentDir)
+        .replace('##links', linksList);
+
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+    });
+    res.end(HTML);
+
+}).listen(3000);
